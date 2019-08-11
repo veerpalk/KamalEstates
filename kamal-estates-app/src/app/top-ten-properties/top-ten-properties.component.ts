3edabdfe338d36f6cbcf867d9acf76ffb5df21cb
davinderdhindsa350 <davinderdhindsa350@gmail.com>
2019-08-11 16:45:25 +0530

    Finnal Demo

--- kamal-estates-app/src/app/top-ten-properties/top-ten-properties.component.ts
+++ kamal-estates-app/src/app/top-ten-properties/top-ten-properties.component.ts
@@ -14,15 +14,28 @@ export class TopTenPropertiesComponent implements OnInit {
 
   constructor() {
 
-    let property1 = new Property({propertyNumber:13653,propertyLocation:'MOHALI',PropertyType:'RESIDENTIAL',PropertyArea:'200.00 SQUARE YARDS',PropertyCost:'55.00 lacs'});
-    let property2 = new Property({propertyNumber:13653,propertyLocation:'MOHALI',PropertyType:'RESIDENTIAL',PropertyArea:'200.00 SQUARE YARDS',PropertyCost:'55.00 lacs'});
-    let property3 = new Property({propertyNumber:13653,propertyLocation:'MOHALI',PropertyType:'RESIDENTIAL',PropertyArea:'200.00 SQUARE YARDS',PropertyCost:'55.00 lacs'});
-    let property4 = new Property({propertyNumber:13653,propertyLocation:'MOHALI',PropertyType:'RESIDENTIAL',PropertyArea:'200.00 SQUARE YARDS',PropertyCost:'55.00 lacs'});
+    let property1 = new Property({propertyNumber:13653,propertyLocation:'MOHALI',PropertyType:'RESIDENTIAL',PropertyArea:'200.00 SQUARE YARDS',PropertyCost:'55.00 lacs',img:'../../assets/img/portfolio/sectorPlan1.jpg'});
+    let property2 = new Property({propertyNumber:13653,propertyLocation:'MOHALI',PropertyType:'RESIDENTIAL',PropertyArea:'200.00 SQUARE YARDS',PropertyCost:'55.00 lacs',img:'../../assets/img/portfolio/sectorPlan2.jpg'});
+    let property3 = new Property({propertyNumber:13653,propertyLocation:'MOHALI',PropertyType:'RESIDENTIAL',PropertyArea:'200.00 SQUARE YARDS',PropertyCost:'55.00 lacs',img:'../../assets/img/portfolio/sectorPlan3.jpg'});
+    let property4 = new Property({propertyNumber:13653,propertyLocation:'MOHALI',PropertyType:'RESIDENTIAL',PropertyArea:'200.00 SQUARE YARDS',PropertyCost:'55.00 lacs',img:'../../assets/img/portfolio/sectorPlan4.jpg'});
+    let property5 = new Property({propertyNumber:13653,propertyLocation:'MOHALI',PropertyType:'RESIDENTIAL',PropertyArea:'200.00 SQUARE YARDS',PropertyCost:'55.00 lacs',img:'../../assets/img/portfolio/sectorPlan5.jpg'});
+    let property6 = new Property({propertyNumber:13653,propertyLocation:'MOHALI',PropertyType:'RESIDENTIAL',PropertyArea:'200.00 SQUARE YARDS',PropertyCost:'55.00 lacs',img:'../../assets/img/portfolio/sectorPlan6.jpg'});
+    let property7 = new Property({propertyNumber:13653,propertyLocation:'MOHALI',PropertyType:'RESIDENTIAL',PropertyArea:'200.00 SQUARE YARDS',PropertyCost:'55.00 lacs',img:'../../assets/img/portfolio/sectorPlan7.jpg'});
+    let property8 = new Property({propertyNumber:13653,propertyLocation:'MOHALI',PropertyType:'RESIDENTIAL',PropertyArea:'200.00 SQUARE YARDS',PropertyCost:'55.00 lacs',img:'../../assets/img/portfolio/sectorPlan8.jpg'});
+    let property9 = new Property({propertyNumber:13653,propertyLocation:'MOHALI',PropertyType:'RESIDENTIAL',PropertyArea:'200.00 SQUARE YARDS',PropertyCost:'55.00 lacs',img:'../../assets/img/portfolio/sectorPlan9.jpg'});
+    let property10 = new Property({propertyNumber:13653,propertyLocation:'MOHALI',PropertyType:'RESIDENTIAL',PropertyArea:'200.00 SQUARE YARDS',PropertyCost:'55.00 lacs',img:'../../assets/img/portfolio/sectorPlan10.jpg'});
  
     this.properties.push(property1);
     this.properties.push(property2);
     this.properties.push(property3);
     this.properties.push(property4);
+    this.properties.push(property4);
+    this.properties.push(property5);
+    this.properties.push(property6);
+    this.properties.push(property7);
+    this.properties.push(property8);
+    this.properties.push(property9);
+    this.properties.push(property10);
 
    }
 
